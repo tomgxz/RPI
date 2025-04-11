2a8f9628ddeed3c55f430b0819635f87d2ca675d
Tom_gxz <75934344+tomgxz@users.noreply.github.com>
2025-04-11 20:10:19 +0100

    Line break

--- lcd.py
+++ lcd.py
@@ -7,4 +7,4 @@ lcd.clear()
 
 lcd.write_string("Help!")
 lcd.cursor_pos = (2, 0)
-lcd.write_string("I am stuck inside this LCD display :(")
+lcd.write_string("I am stuck inside\n\rthis LCD display :(")
